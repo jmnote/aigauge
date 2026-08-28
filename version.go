b40a13dd5d3b75ac1129f83e38dc35b3b912d4ce
Jmnote <opcore@gmail.com>
2026-08-29 00:17:32 +0900

    Read application version from VERSION file

--- version.go
+++ version.go
@@ -1,9 +1,13 @@
 package main
 
-// appVersion is the single source of truth for the application version.
-// Release builds can override it with: -ldflags "-X main.appVersion=1.0.0"
-var appVersion = "0.1.0"
+import (
+	_ "embed"
+	"strings"
+)
+
+//go:embed VERSION
+var versionFile string
 
 func (a *App) GetVersion() string {
-	return appVersion
+	return strings.TrimSpace(versionFile)
 }
